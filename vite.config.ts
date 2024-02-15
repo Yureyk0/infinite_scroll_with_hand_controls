02e7de52869a726fccc831b8291a7066b8b689a9
Yura Boreyko <yuraboreyko@gmail.com>
2024-02-15 14:18:39 +0200

    added base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   build: {
     outDir: './build',
-    rollupOptions:{
-      
-    }
   }
 
 
