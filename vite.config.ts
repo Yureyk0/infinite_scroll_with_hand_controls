6508f411ceef211d52f61f9496908292eba0fa43
Yura Boreyko <yuraboreyko@gmail.com>
2024-02-15 13:52:47 +0200

    fix root path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: './build'
+    outDir: './build',
+    rollupOptions:{
+      
+    }
   }
 
 
